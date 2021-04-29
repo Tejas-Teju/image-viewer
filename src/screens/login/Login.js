951fd9ee0e8826ce24e8564e47953ebccdb93120
Tejas-Teju <tejas8196@gmail.com>
2021-04-30 00:44:59 +0530

    Added Card component with heading as LOGIN

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,11 +1,22 @@
-import React, { Component } from "react";
-import Header from '../common/header/Header'
+import React, { Component } from 'react';
+import Header from '../common/header/Header';
+import './Login.css';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
 class Login extends Component {
     render() {
-        return (
+        return <div>
             <Header />
-        )
+            <Card className="cardStyle">
+              <CardContent>
+                <Typography variant="h5" gutterBottom>
+                  LOGIN
+                </Typography>
+              </CardContent>
+            </Card>
+          </div>;
     }
 }
 
