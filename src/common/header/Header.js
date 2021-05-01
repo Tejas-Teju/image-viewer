52c3619d8386f8d2773915061a0f06cff5149b5b
Tejas-Teju <tejas8196@gmail.com>
2021-05-01 23:33:23 +0530

    Added search box without search functionality

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,16 +1,38 @@
 import React, { Component } from "react";
 import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import Input from '@material-ui/core/Input';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = (theme => ({
+    searchText: {  //seach text styling 
+        position: "relative",
+        width: "100%",
+        padding: "5px 0px 0px 5px",
+    },
+}));
 
 class Header extends Component {
     render() {
+        //custom Styles are stored in classes
+        const { classes } = this.props;
+
         return (
             <div>
-               <header className="app-header">
-                    <p className="logo">Image Viewer</p>
+                <header className="app-header">
+                    <a href='/' id="logo">Image Viewer</a>
+                    {/* Search box shown only when showSearchBox is true */} 
+                    {this.props.showSearchBox ?                  
+                        <span className="header-searchbox">
+                            <SearchIcon id="search-icon"></SearchIcon>
+                            <Input className={classes.searchText} placeholder="Search…" disableUnderline={true} />
+                        </span>
+                        : <span></span> //Do not display search box
+                    }
                 </header>
             </div>
         )
     }
 }
 
-export default Header;
+export default withStyles(styles)(Header);
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,9 +1,20 @@
 import React, {Component} from 'react';
+import Header from '../../common/header/Header';
 
 class Home extends Component {
+    constructor() {
+        super();
+        this.state = {
+            isLoggedIn: sessionStorage.getItem("access-token") == null ? false : true,
+        };
+
+    }
+
     render() {
         return(
-            <div>Home Page</div>
+            <div>
+                <Header showSearchBox={this.state.isLoggedIn ? true : false} showProfileIcon={this.state.isLoggedIn ? true : false} />
+            </div>
         );
     }
 }
