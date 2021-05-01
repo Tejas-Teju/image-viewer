37bcb21be3e4db2dcbb1d6c0b59578b95b5a864c
Tejas-Teju <tejas8196@gmail.com>
2021-05-02 00:11:48 +0530

    Added profile picture as icon in header

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import Input from '@material-ui/core/Input';
+import IconButton from '@material-ui/core/IconButton';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = (theme => ({
@@ -27,7 +28,17 @@ class Header extends Component {
                             <SearchIcon id="search-icon"></SearchIcon>
                             <Input className={classes.searchText} placeholder="Search…" disableUnderline={true} />
                         </span>
-                        : <span></span> //Do not display search box
+                        : "" //Do not display search box
+                    }
+
+                    {/* Profile picture is shown only when showProfileIcon is true */} 
+                    {this.props.showProfileIcon ?  
+                        <span>
+                            <IconButton id="profile-icon">
+                                <img src="https://scontent.cdninstagram.com/v/t51.29350-15/133727129_826954304812183_722079783531579720_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=8ae9d6&_nc_ohc=rdMfksgtV_cAX85t6nn&_nc_ht=scontent.cdninstagram.com&oh=74ab3881a52024243b3f4ab9ac0e2f2d&oe=60B12CC4" alt="profileImage" id="profile-picture" />
+                            </IconButton>
+                        </span>
+                        : "" //Do not display profile picture
                     }
                 </header>
             </div>
